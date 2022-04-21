17c832b554a02afe95a570b63cf747b232553113
viktolim <s039891@ad.viko.lt>
2022-04-21 20:36:26 +0300

    error messages to sign up

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser= require("body-parser");
 const ejs= require("ejs");
+const { use } = require("express/lib/application");
 
 const homePageInfo= "Hello to see you";
 const aboutPage= "It's good to see you";
 const aboutContact= "see you on my website";
+const userInfo=[];
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -16,12 +18,30 @@ app.get("/", function(req, res){
     res.render("template");
 });
 app.get("/signup", function(req,res){
-    res.render("registration");
+    res.render("registration",{User:"",Error:false,Msg:null});
 });
-app.post("/registration", function(req,res){
-    console.log(req.body.firstName);
-    res.redirect("/");
-   
+app.get("/login", function(req,res){
+    res.render("login");
+});
+app.post("/signup", function(req,res){
+    
+    var msg=null;
+    var user={
+        name:req.body.firstName,
+        lastname: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password
+    };
+    if(req.body.password!==req.body.password2){
+        msg="Slaptažodžiai nėra vienodi"
+    }
+    else if(req.body.password!==""){msg="Įveskite slaptažodį";}
+    if(req.body.password2!=="")msg="Pakartokite slaptažodį";
+    
+    res.render("registration",{User:user,Error:true,Msg:msg});
+});
+app.post("/login", function(req,res){
+    console.log(req.body.pasword);
 });
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
